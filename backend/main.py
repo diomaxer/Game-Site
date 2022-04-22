36de1d160298293403bb85b70885407b696be3d0
diomaxer <diomaxer@gmail.com>
2022-04-22 18:01:39 +0300

    cors

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,21 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 from backend.routers import games_router
 
 
 app = FastAPI()
 
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(router=games_router.router, prefix="/v1/games", tags=["games"])
